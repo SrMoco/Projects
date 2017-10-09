dbb93d92a285ddd7360d9a0ecdb58c65c5d9c388
Dentvar <marius.paas@gmx.de>
2017-10-09 02:16:45 +0200

    Probando
    Con el cliente nativo de Windwos
    Añado 7_1_exercice.c

---
+++ 7_1_exercise.c
@@ -0,0 +1,24 @@
+/* Program 7.1 A simple program using pointers */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 0;
+    int *pointer = NULL;
+
+    printf("\nnumber's adress: %p", &number);
+    printf("\nnumber's value %d",number);
+    printf("\nnumber's size %d bytes\n\n", sizeof(number));
+
+    pointer =&number;
+
+    printf("pointer's adress: %p", &pointer);
+    printf("\npointer's size %d bytes", sizeof(pointer));
+    printf("\npointer's value: %p", pointer);
+    printf("\nvalue pointed to %d\n", *pointer);
+
+
+    return 0;
+}
